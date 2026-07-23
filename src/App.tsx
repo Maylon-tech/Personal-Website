c4e72402567f576b3ce80b3a290e993d69b0cbc3
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-07-23 15:08:52 +0900

    config project sheet done, create component form shad now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+/*
+@copyright 2026 NemotoDev
+@licence Apache-2.0
+*/
 
+import {
+  Button
+} from "@/components/ui/button"
+ 
 
 function App() {
   
   return (
     <>
-      Hello Nemoto
+      <Button>Click me</Button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+/*
+@copyright 2026 NemotoDev
+@licence Apache-2.0
+*/
+
+
+// Node Modules
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+// Styles
+import '@/index.css'
+
+// Components
+import App from '@/App'
+import Sidebar from '@/components/Sidebar'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <main className="min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10">
+      <Sidebar />
+      <App />
+    </main>
   </StrictMode>,
 )
